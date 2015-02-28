c72f17a7a90c284b83c40aad01eae9b2d7f40911
Pavel Mazurin <kovpas@gmail.com>
2015-02-28 12:31:10 +0100

    Initial Commit

---
+++ docopt/docopt.h
@@ -0,0 +1,19 @@
+//
+//  docopt.h
+//  docopt
+//
+//  Created by Pavel S. Mazurin on 2/28/15.
+//  Copyright (c) 2015 kovpas. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for docopt.
+FOUNDATION_EXPORT double docoptVersionNumber;
+
+//! Project version string for docopt.
+FOUNDATION_EXPORT const unsigned char docoptVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <docopt/PublicHeader.h>
+
+
