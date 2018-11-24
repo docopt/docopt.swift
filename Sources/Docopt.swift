550ea32104f82942c4d6577858077d09c9265838
Ludovico Rossi <ludo@icecreamstudios.com>
2018-11-24 17:22:20 +0000

    Fix warning with Swift 4.2 / Xcode 10 (#16)
    "Static declarations are implicitly 'final'; use 'public' instead of 'open'"

--- Sources/Docopt.swift
+++ Sources/Docopt.swift
@@ -18,7 +18,7 @@ open class Docopt : NSObject {
     fileprivate let optionsFirst: Bool
     fileprivate let arguments: [String]
     
-    @objc open static func parse(_ doc: String, argv: [String], help: Bool = false, version: String? = nil, optionsFirst: Bool = false) -> [String: AnyObject] {
+    @objc public static func parse(_ doc: String, argv: [String], help: Bool = false, version: String? = nil, optionsFirst: Bool = false) -> [String: AnyObject] {
         return Docopt(doc, argv: argv, help: help, version: version, optionsFirst: optionsFirst).result
     }
     
